178fef96a0bb2d842d7f3e9b1a145dc39ce60da6
Andreas Tondorf <atondorf@sig.biz>
2023-03-02 22:07:45 +0100

    Initial empty project ...

---
+++ extension/src/main/java/twx/pdf/PDFExport.java
@@ -0,0 +1,30 @@
+package twx.pdf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.thingworx.data.util.InfoTableInstanceFactory;
+import com.thingworx.entities.utils.ThingUtilities;
+import com.thingworx.logging.LogUtilities;
+import com.thingworx.metadata.annotations.ThingworxServiceDefinition;
+import com.thingworx.metadata.annotations.ThingworxServiceParameter;
+import com.thingworx.metadata.annotations.ThingworxServiceResult;
+import com.thingworx.resources.Resource;
+import com.thingworx.things.repository.FileRepositoryThing;
+import com.thingworx.types.InfoTable;
+import com.thingworx.types.collections.ValueCollection;
+import com.thingworx.types.primitives.StringPrimitive;
+
+public class PDFExport {
+
+}
